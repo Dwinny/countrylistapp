479939bbe8d34cd4c1b73be580d67db5e83637d9
chioma <chiomag58@gmail.com>
2022-12-07 11:55:21 +0100

    new commit

---
+++ lib/screens/details_screen/index.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:i_country/model/country_model.dart';
+// import '../details_screen/component/body.dart';
+
+// class DetailsScreen extends StatelessWidget {
+//   const DetailsScreen({super.key, required Country model});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Body();
+//   }
+// }
